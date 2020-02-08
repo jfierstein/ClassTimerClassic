d4147ba75be5a95ae59ade20dd89bca87f0fe566
Josh Fierstein <josh.fierstein@gmail.com>
2020-02-07 19:00:38 -0500

    updating warlock spell Ids

--- Bars/Warlock.lua
+++ Bars/Warlock.lua
@@ -8,38 +8,30 @@ return {
 		GetSpellInfo(1098), -- Enslave Demon
 		GetSpellInfo(1122), -- Summon Infernal
 		GetSpellInfo(17941), -- Nightfall
-        GetSpellInfo(140074), -- Molten Core
 		(GetSpellInfo(17794)), -- Shadow Vulnerability --Important: Double parentheses are necessary because the last item in a table contains all the values from the function call and we only want the first one.
 	},
 	Curses = {
 		GetSpellInfo(980), -- Agony
+		GetSpellInfo(17937), -- Shadows
+		GetSpellInfo(11722), -- Elements
 		(GetSpellInfo(603)), -- Doom --Important: Double parentheses are necessary because the last item in a table contains all the values from the function call and we only want the first one.
 	},
 	DOTs = {
 		GetSpellInfo(172), -- Corruption
-		GetSpellInfo(44518), -- Immolate
-		GetSpellInfo(61290), -- Shadowflame
-		GetSpellInfo(27243), -- Seed of Corruption
-		GetSpellInfo(48181), -- Haunt
-		GetSpellInfo(47960), -- Shadowflame
+		GetSpellInfo(25309), -- Immolate
 		GetSpellInfo(17962), -- Conflagrate
 		GetSpellInfo(124480), -- Conflag, green
 		GetSpellInfo(124481), -- Conflag, green, ae
-		GetSpellInfo(689), -- Drain Life
-		GetSpellInfo(80240), -- Havoc
-		(GetSpellInfo(30108)), -- Unstable Affliction --Important: Double parentheses are necessary because the last item in a table contains all the values from the function call and we only want the first one.
+		(GetSpellInfo(689)), -- Drain Life
 	},
 	Misc = {
 		GetSpellInfo(710), -- Banish
-		GetSpellInfo(48184), --Haunt
 		GetSpellInfo(6789), -- Mortal Coil
 		GetSpellInfo(5782), -- Fear
 		GetSpellInfo(5484), -- Howl of Terror
-		GetSpellInfo(29893), -- Ritual of Souls
 		GetSpellInfo(6358), -- Seduction
-		GetSpellInfo(17877), -- Shadowburn
+		GetSpellInfo(18878), -- Shadowburn
 		(GetSpellInfo(20707)), -- Soulstone Resurrection --Important: Double parentheses are necessary because the last item in a table contains all the values from the function call and we only want the first one.
 	}
 }
 end
-
